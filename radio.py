29b3b2f2fb435d564b26b46183e8accfddc4118a
Aleksi Illikainen <illikainen.aleksi@gmail.com>
2019-10-26 18:15:52 +0300

    Lisättiin uusia kanavia, ja nyt näkee mikä kanava soi. GUI on myös paremman näköinen, ja GUI:ssa on tyhjä canvas, johon tulee tulevaisuudessa jotain.

--- radio.py
+++ radio.py
@@ -7,7 +7,10 @@ CHANNELS = {
 	"SUOMI": "http://stream.bauermedia.fi/suomirock/suomirock_64.aac", 
 	"NRJ": "http://cdn.nrjaudio.fm/adwz1/fi/35001/mp3_128.mp3",
 	"ISKELMÄ": "http://stream.bauermedia.fi/iskelma/iskelma_64.aac",
-	"PUHE": "https://yleuni-f.akamaihd.net/i/yleliveradiohd_5@113882/segment157203920_128_a-p.ts?sd=10&rebase=on"
+	"PUHE": "https://yleuni-f.akamaihd.net/i/yleliveradiohd_5@113882/index_128_a-p.m3u8?sd=10&rebase=on",
+	"HELMI-RADIO": "https://digitacdn.akamaized.net/hls/live/629243/radiohelmi/master-128000.m3u8",
+	"RADIO ROCK": "https://digitacdn.akamaized.net/hls/live/629243/radiorock/master.m3u8",
+	"SUOMI-POP": "https://digitacdn.akamaized.net/hls/live/629243/radiosuomipop/master-128000.m3u8"
 }
 
 CHANNEL_URL = CHANNELS["YLEX"]
@@ -20,13 +23,13 @@ def init_Window(title_name="Radio"):
 	window.resizable(0, 0)
 	return window
 	
-def create_Label(string, window, x, y):
-	return tkinter.Label(window, text = string).grid(row = y, column = x)
+def create_Label(window, x, y, var):
+	return tkinter.Label(window, textvariable=var).grid(row = y, column = x)
 	
-def button_Handler(name):
+def button_Handler(name, var):
 	global CHANNEL_URL
 	CHANNEL_URL = CHANNELS[name]
-	print(name)
+	var.set(name)
 	PLAY[0] = False
 	
 def init_Frames(window):
@@ -34,35 +37,44 @@ def init_Frames(window):
 	bot_frame = tkinter.Frame(window)
 	return top_frame, bot_frame
 		
-def create_Button(string, color, y, x):
-	return tkinter.Button(text = string, fg = color, width = 14, height = 6, command=lambda: button_Handler(string)).grid(row = y, column = x)
+def create_Button(string, color, y, x, var):
+	return tkinter.Button(text = string, fg = color, width = 14, height = 6, command=lambda: button_Handler(string, var)).grid(row = y, column = x)
 	
 def main():
+	quit = False
 	Instance = vlc.Instance("--input-repeat=-1", "--fullscreen")
 	Player = Instance.media_player_new()
-	while True:
+	while not quit:
 		Media = Instance.media_new(CHANNEL_URL)
 		Player.set_media(Media)
 		Player.play()
 		while True:
 			if (PLAY[0] == False):
 				Player.stop()
-				Instance = vlc.Instance("--input-repeat=-1", "--fullscreen")
-				Player = Instance.media_player_new()
 				Media = Instance.media_new(CHANNEL_URL)
 				Player.set_media(Media)
 				Player.play()
 				PLAY[0] = True
-			window.update_idletasks()
-			window.update()
+			try:
+				window.update_idletasks()
+				window.update()
+			except tkinter.TclError:
+				quit = True
+				break
 		
 if __name__ == "__main__":
 	window = init_Window()
-	canvas = tkinter.Canvas(window, bg = "black", width = 100, height = 50).grid(row = 0 , column = 0)	
-	button1 = create_Button("NOVA", "black", 1, 0)
-	button2 = create_Button("YLEX", "black", 1, 1)
-	button3 = create_Button("SUOMI", "black", 1, 2)
-	button4 = create_Button("PUHE", "black", 2, 0)
-	button5 = create_Button("NRJ", "black", 2, 1)
-	button6 = create_Button("ISKELMÄ", "black", 2, 2)
+	canvas = tkinter.Canvas(window, height = 150, width = 300, bg="black").grid(row = 0, columnspan = 3)
+	var = tkinter.StringVar()
+	label1 = create_Label(window, 1, 1, var)
+	var.set("YLEX")
+	button1 = create_Button("NOVA", "black", 2, 0, var)
+	button2 = create_Button("YLEX", "black", 2, 1, var)
+	button3 = create_Button("SUOMI", "black", 2, 2, var)
+	button4 = create_Button("PUHE", "black", 3, 0, var)
+	button5 = create_Button("NRJ", "black", 3, 1, var)
+	button6 = create_Button("ISKELMÄ", "black", 3, 2, var)
+	button7 = create_Button("HELMI-RADIO", "black", 4, 0, var)
+	button8 = create_Button("RADIO ROCK", "black", 4, 1, var)
+	button9 = create_Button("SUOMI-POP", "black", 4, 2	, var)
 	main()
